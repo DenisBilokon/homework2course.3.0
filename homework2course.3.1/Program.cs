0d1b658836ef665dc43c600a73f65bff3a134cb6
DenisBilokon <106659593+DenisBilokon@users.noreply.github.com>
2023-02-25 19:36:10 +0200

    Update Program.cs

--- homework2course.3.1/Program.cs
+++ homework2course.3.1/Program.cs
@@ -9,21 +9,17 @@ namespace TransactionAnalyzer
     {
         static void Main(string[] args)
         {
-            // Шлях до файлу CSV та формат дати
+            
             string csvFilePath = "transactions.csv";
             string dateFormat = "dd/MM/yyyy";
-
-            // Функції для отримання дати та суми з записів CSV файлу
             Func<string, DateTime> getDate = line => DateTime.ParseExact(line.Split(',')[0], dateFormat, null);
             Func<string, double> getAmount = line => double.Parse(line.Split(',')[1]);
 
-            // Делегат для відображення загальної суми витрат за кожен день
             Action<DateTime, double> displayDailyTotal = (date, total) =>
             {
                 Console.WriteLine($"За день {date.ToString(dateFormat)} було витрачено {total.ToString("C")}");
             };
 
-            // Читання записів з CSV файлу та групування їх за датою
             var transactionsByDate = new Dictionary<DateTime, List<double>>();
             int lineCount = 0;
 
@@ -46,22 +42,18 @@ namespace TransactionAnalyzer
 
                     if (lineCount % 10 == 0)
                     {
-                        // Збереження даних кожних 10 записів у файл
                         SaveDataToFile(transactionsByDate, csvFilePath, dateFormat);
 
-                        // Відображення загальної суми витрат за кожен день
                         foreach (var entry in transactionsByDate)
                         {
                             displayDailyTotal(entry.Key, entry.Value.Sum());
                         }
 
-                        // Очищення словника транзакцій
                         transactionsByDate.Clear();
                     }
                 }
             }
 
-            // Збереження останніх записів у файл та відображення загальної суми витрат за кожен день
             SaveDataToFile(transactionsByDate, csvFilePath, dateFormat);
 
             foreach (var entry in transactionsByDate)
